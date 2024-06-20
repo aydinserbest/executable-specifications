d16019e88e5352bc8c505c7f4903bea8fdee4c20
octocat <aydinserbest34@gmail.com>
2024-06-21 01:00:03 +0200

    added cucumber report configuration

--- src/test/java/starter/AcceptanceTestSuite.java
+++ src/test/java/starter/AcceptanceTestSuite.java
@@ -6,13 +6,12 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "starter")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
+@ConfigurationParameter(key = "cucumber.publish.enabled", value = "true")
 
 public class AcceptanceTestSuite {
 }
