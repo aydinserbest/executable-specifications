7d4e1f25d9320b0423aa66ae9b907b7b157e4e90
octocat <aydinserbest34@gmail.com>
2024-06-25 13:57:37 +0200

    created simple junit5 branch

---
+++ src/test/java/starter/SimpleJunit5Test.java
@@ -0,0 +1,12 @@
+package starter;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleJunit5Test {
+    @Test
+    void mySimpleTest() {
+        // My test logic here
+        System.out.println("slm");
+    }
+
+}
--- src/test/resources/features/hello.feature
+++
@@ -1,7 +0,0 @@
-Feature: Greetings
-
-  The different types of greetings we can receive
-
-  Scenario: Hello How Are You
-    When I am greeted
-    Then I should hear "Hello How Are You?"
