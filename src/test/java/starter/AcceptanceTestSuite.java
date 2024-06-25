5aab1af4afab05d3e2f14582e929480b704601f6
octocat <aydinserbest34@gmail.com>
2024-06-25 10:54:23 +0200

    added explanation lines to the runner test class

--- src/test/java/starter/AcceptanceTestSuite.java
+++ src/test/java/starter/AcceptanceTestSuite.java
@@ -7,9 +7,9 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("features")
-//@ConfigurationParameter(key = "cucumber.publish.enabled", value = "true")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@SelectPackages("features")  //if you comment out this line, NoTestsDiscoveredException: Suite [starter.AcceptanceTestSuite] did not discover any tests error occurs
+//@ConfigurationParameter(key = "cucumber.publish.enabled", value = "true") can be used instead of .properties file
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty") // to see feature steps on the console
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "starter") //the package where your runner step definitions are
 
 public class AcceptanceTestSuite {
