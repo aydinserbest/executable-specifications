75823f33c9a9daa618ec34cbe5539b96093942a7
octocat <aydinserbest34@gmail.com>
2024-06-25 14:07:55 +0200

    created simple junit4 branch

--- src/test/java/starter/SimpleJunit5Test.java
+++ src/test/java/starter/SimpleJunit5Test.java
@@ -1,10 +1,11 @@
 package starter;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 public class SimpleJunit5Test {
     @Test
-    void mySimpleTest() {
+    public void mySimpleTest() {
         // My test logic here
         System.out.println("slm");
     }
