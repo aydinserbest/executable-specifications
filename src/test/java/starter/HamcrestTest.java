70f29242dacc1c4f218a02947fe4e31900dd955d
octocat <aydinserbest34@gmail.com>
2024-06-29 21:08:22 +0200

    added some annotations

---
+++ src/test/java/starter/HamcrestTest.java
@@ -0,0 +1,19 @@
+package starter;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Tag("hamcrest")
+public class HamcrestTest {
+    @Test
+    void assertWithHamcrestMatcher() {
+        assertThat(2 + 1, equalTo(3));
+        assertThat("Foo", notNullValue());
+        assertThat("Hello world", containsString("world"));
+    }
+
+
+}
--- src/test/java/starter/SimpleJunit5Test.java
+++ src/test/java/starter/SimpleJunit5Test.java
@@ -1,12 +1,20 @@
 package starter;
 
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SimpleJunit5Test {
     @Test
-    void mySimpleTest() {
+    void mySimpleTest1() {
         // My test logic here
-        System.out.println("slm");
+        System.out.println("slm1");
+    }
+    @Disabled("mySimpleTest2 disabled") //when we run test, it does not work only text is seen in output
+    @Test
+    void mySimpleTest2() {
+        // My test logic here
+        System.out.println("slm2");
     }
 
 }
