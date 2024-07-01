7eaf6232f3fd6d8f8d67f5f6bfde39df999bb877
octocat <aydinserbest34@gmail.com>
2024-07-01 18:04:09 +0200

    added minor changes

--- src/test/java/starter/SimpleJunit5Test.java
+++ src/test/java/starter/SimpleJunit5Test.java
@@ -1,9 +1,11 @@
 package starter;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SimpleJunit5Test {
     @Test
+    @DisplayName("slm")
     void mySimpleTest() {
         // My test logic here
         System.out.println("slm");
