57a8a46b5a43494d80a0d5ea2758092c1097c3c7
octocat <aydinserbest34@gmail.com>
2024-07-01 18:02:45 +0200

    added minor changes

--- src/main/java/starter/Belly.java
+++ src/main/java/starter/Belly.java
@@ -1,7 +1,17 @@
 package starter;
 
 public class Belly {
+    private int cukes;
     public void eat(int cukes) {
+        this.cukes = cukes;
+    }
+
+    public void waitTime(int hours) {
+        // Simulate waiting time, you can expand this if needed
+    }
+
+    public String growl() {
+        return cukes > 0 ? "Growl" : "Silent";
 
     }
 }
--- src/test/java/starter/BellyStepDef.java
+++ src/test/java/starter/BellyStepDef.java
@@ -1,11 +1,32 @@
 package starter;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BellyStepDef {
+    private Belly belly;
+
     @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
+    @DisplayName("Have cukes in belly")
+    public void i_have_cukes_in_my_belly(int cukes) {
+        belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    @DisplayName("Wait for some time")
+    public void i_wait_hour(int hours) {
+        belly.waitTime(hours);
+    }
+
+    @Then("my belly should growl")
+    @DisplayName("Belly should growl")
+    public void my_belly_should_growl() {
+        assertEquals("Growl", belly.growl());
     }
 }
