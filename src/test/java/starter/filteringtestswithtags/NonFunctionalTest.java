53d8ba20ad1b4ca9f2920abf2f6f304060d29226
octocat <aydinserbest34@gmail.com>
2024-07-02 13:43:01 +0200

    still have some issues with execution of tags

--- src/test/java/starter/filteringtestswithtags/NonFunctionalTest.java
+++ src/test/java/starter/filteringtestswithtags/NonFunctionalTest.java
@@ -7,24 +7,18 @@ import org.junit.jupiter.api.Test;
 public class NonFunctionalTest {
 
     @Test
-    @Tag("performance")
-    @Tag("load")
     void testOne() {
         System.out.println("Non-Functional Test 1 (Performance/Load)");
     }
     @Test
-    @Tag("performance")
-    @Tag("stress")
     void testTwo() {
         System.out.println("Non-Functional Test 2 (Performance/Stress)");
     }
     @Test
-    @Tag("security")
     void testThree() {
         System.out.println("Non-Functional Test 3 (Security)");
     }
     @Test
-    @Tag("usability")
     void testFour() {
         System.out.println("Non-Functional Test 4 (Usability)"); }
 
